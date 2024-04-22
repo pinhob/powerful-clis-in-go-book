dc29a43a2259fd5271d405e4039facc5263df23d
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-22 18:15:12 -0300

    feat (ch01): create count func

--- wc/main.go
+++ wc/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	wordCount := count(os.Stdin)
+	fmt.Println(wordCount)
+}
+
+func count(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+
+	scanner.Split(bufio.ScanWords)
+
+	wc := 0
+
+	for scanner.Scan() {
+		wc++
+	}
+
+	return wc
 }
