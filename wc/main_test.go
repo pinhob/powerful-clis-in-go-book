278372630da9d7fd392698154a38921e9c3bfaae
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-22 19:04:53 -0300

    feat (ch01): add AssertCount helper func

--- wc/main_test.go
+++ wc/main_test.go
@@ -7,33 +7,40 @@ import (
 
 func TestCountWords(t *testing.T) {
 	input := bytes.NewBufferString("word1 word2 word3 word4\n")
+	want := 4
 
-	exp := 4
-	res := count(input, false, false)
-
-	if res != exp {
-		t.Errorf("Expected %d, got %d instead.\n", exp, res)
-	}
+	AssertCount(t, input, want, "")
 }
 
 func TestCountLines(t *testing.T) {
 	input := bytes.NewBufferString("word1\nword2\nword3")
+	want := 3
 
-	exp := 3
-	res := count(input, true, false)
-
-	if res != exp {
-		t.Errorf("Expected %d, got %d instead.\n", exp, res)
-	}
+	AssertCount(t, input, want, "l")
 }
 
 func TestCountBytes(t *testing.T) {
 	input := bytes.NewBufferString("word1")
-
 	exp := 5
-	res := count(input, false, true)
 
-	if res != exp {
-		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	AssertCount(t, input, exp, "b")
+}
+
+func AssertCount(t testing.TB, input *bytes.Buffer, want int, flagType string) {
+	t.Helper()
+
+	var got int
+
+	switch flagType {
+	case "l":
+		got = count(input, true, false)
+	case "b":
+		got = count(input, false, true)
+	default:
+		got = count(input, false, false)
+	}
+
+	if got != want {
+		t.Errorf("Expected %d, got %d instead.\n", got, want)
 	}
 }
