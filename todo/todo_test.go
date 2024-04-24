eb2558f1444a8ce41e0331403475978c3921f87a
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-24 15:03:29 -0300

    test (ch02): refactor tests

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -14,6 +14,9 @@ func TestAdd(t *testing.T) {
 	if len(list) != 1 {
 		t.Errorf("got %d, want 1", len(list))
 	}
+	if list[0].Task != task {
+		t.Errorf("Expected %q, got %q instead", task, list[0].Task)
+	}
 }
 
 func TestComplete(t *testing.T) {
@@ -22,6 +25,10 @@ func TestComplete(t *testing.T) {
 	taskNumber := 1
 
 	list.Add(task)
+
+	if list[taskNumber-1].Task != task {
+		t.Errorf("Expected %q, got %q instead", task, list[0].Task)
+	}
 
 	if list[taskNumber-1].Done {
 		t.Errorf("Task should not be complete")
@@ -43,18 +50,33 @@ func TestComplete(t *testing.T) {
 
 func TestDelete(t *testing.T) {
 	list := todo.List{}
-	task := "todo"
 	taskNumber := 1
 
-	list.Add(task)
+	tasks := []string{
+		"task 1",
+		"task 2",
+		"task 3",
+	}
 
-	if len(list) != 1 {
-		t.Errorf("list should have the length of 1, but got length of %d", len(list))
+	for _, task := range tasks {
+		list.Add(task)
+	}
+
+	if len(list) != 3 {
+		t.Errorf("list should have the length of 3, but got length of %d", len(list))
+	}
+
+	if list[0].Task != tasks[0] {
+		t.Errorf("Expected %q, got %q instead", tasks[0], list[0].Task)
 	}
 
 	list.Delete(taskNumber)
 
-	if len(list) != 0 {
+	if len(list) != 2 {
 		t.Errorf("list should have the length of 0, but got length of %d", len(list))
+	}
+
+	if list[1].Task != tasks[2] {
+		t.Errorf("Expected %q, got %q instead", tasks[2], list[1].Task)
 	}
 }
