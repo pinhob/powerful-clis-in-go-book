9d5683f2f849dd9fd80cd5349a84461df26bc0dd
pinhob <20286747+pinhob@users.noreply.github.com>
2024-04-23 22:33:53 -0300

    refactor (ch02): TestAdd

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -2,21 +2,13 @@ package todo
 
 import (
 	"testing"
-	"time"
 )
 
 func TestAdd(t *testing.T) {
 	list := List{}
 	task := "todo"
 
-	item := item{
-		Task:        task,
-		Done:        false,
-		CreatedAt:   time.Now(),
-		CompletedAt: time.Time{},
-	}
-
-	list = append(list, item)
+	list.Add(task)
 
 	if len(list) != 1 {
 		t.Errorf("got %d, want 1", len(list))
