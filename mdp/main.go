20d3478a6e21e912039eb8173063b6050383ea67
pinhob <20286747+pinhob@users.noreply.github.com>
2024-05-24 17:52:10 -0300

    feat (ch03): create parseContent and saveHTML functions

--- mdp/main.go
+++ mdp/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
 )
 
 const (
@@ -29,7 +33,7 @@ func main() {
 	}
 
 	if err := run(*filename); err != nil {
-		fmt.Fprintln(os.Stderr, "got errorr running the file: %v", err)
+		fmt.Fprintln(os.Stderr, "got error running the file: %v", err)
 		os.Exit(1)
 	}
 }
@@ -46,4 +50,24 @@ func run(filename string) error {
 	fmt.Println(outName)
 
 	return saveHTML(outName, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	// Parse the mardown file though blackfriday and bluemonday to generate a valid and safe HTML
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	// Create a buffer of bytes to write to file
+	var buffer bytes.Buffer
+
+	// Write html to bytes buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHTML(outFname string, data []byte) error {
+	return os.WriteFile(outFname, data, 0644)
 }
