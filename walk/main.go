84f3abfbec44f7bc9c198df8ef386c9dc95db910
pinhob <20286747+pinhob@users.noreply.github.com>
2024-06-14 12:41:25 -0300

    feat(walk): create list files and filter options

---
+++ walk/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type config struct {
+	// extension to filter out
+	ext string
+	// min file size
+	size int64
+	// list files
+	list bool
+}
+
+func main() {
+	root := flag.String("root", ".", "Root directory to start")
+	list := flag.Bool("list", false, "List files only")
+	ext := flag.String("ext", "", "File extension to filter out")
+	size := flag.Int64("size", 0, "Minimum file size")
+
+	flag.Parse()
+
+	c := config{
+		ext:  *ext,
+		size: *size,
+		list: *list,
+	}
+
+	if err := run(*root, os.Stdout, c); err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(root string, out io.Writer, cfg config) error {
+	return filepath.Walk(root,
+		func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if filterOut(path, cfg.ext, cfg.size, info) {
+				return nil
+			}
+
+			// If list was explicitly set, don't do anything else
+			if cfg.list {
+				return listFile(path, out)
+			}
+
+			return listFile(path, out)
+		})
+}
